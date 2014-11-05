2750445bcaec74ae2fd318c9944cedd58de5b15a
kleinab <kleinab@mit.edu>
2014-11-05 12:28:01 -0500

    Improving documentation and README file.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 JSChecker
 =========
+
+Esprima vs. Acorn:
+---------
+I decided to choose esprima as my js parser. I tried the performance test on esprima's website (http://esprima.org/test/compare.html) and esprima performed slightly better than acorn.
+Additionally, its documentation is more user friendly than acorn's. Acorn displays its entire code base and documents every line. While this is useful to see how short the document is, it's too much information. Esprima's documentation is much easier to "parse" (haha).
+Also, acorn does not provide any examples, while esprima provides several. Finally, esprima describes the form of its output, while acorn does not. Presumably they are the same output, but it would be great to know this ahead of time before running the code.
+
+Testing the API:
+---------
+Change the variables whitelistReqs, blacklistReqs, and structureReqs.
+
+whitelistReqs and blacklistReqs must take the form of a list of types, with the same official names that the esprima parser uses to identify types (for example, VariableDeclaration, FunctionDeclaration, ForStatement, BlockStatement, ReturnStatement).
+
+structureReqs must take the form of a modified esprima ast. Every node is of the form {"type": typeName} and has an optional attribute "body", which may have a single node or a list of nodes as its value.
--- jschecker.js
+++ jschecker.js
@@ -2,6 +2,8 @@
 * Requires:
 ** ast is an AST object produced by esprima.parse()
 ** reqs is a list of type names (for example, ["ForStatement", "VariableDeclaration", "FunctionDeclaration"]) which must be found in the ast. Importantly, reqs cannot repeat types. For example ["ForStatement", "ForStatement"] is not allowed.
+* Returns:
+** List of requirements that are not present in the code. Empty list if all requirements are present in code.
 */
 var whitelist = function(ast, reqs) {
   /*
@@ -14,7 +16,7 @@ var whitelist = function(ast, reqs) {
   /*
   * matches starts off as a copy of reqs. When we find a branch whose type is in reqs, remove it from the list of matches and return the new list.
   */
-  var checkMatch = function(reqs, matches, type) {
+  var updateMatches = function(reqs, matches, type) {
     if (reqs.indexOf(type) != -1) {
       var typeIndex = matches.indexOf(type);
       var matchesCopy = matches.slice(0);
@@ -25,13 +27,15 @@ var whitelist = function(ast, reqs) {
   };
 
   var matches = reqs.slice(0);
-  return dfs(ast, reqs, matches, checkMatch, checkDone);
+  return dfs(ast, reqs, matches, updateMatches, checkDone);
 };
 
 /*
 * Requires:
 ** ast is an AST object produced by esprima.parse()
 ** reqs is a list of type names (for example, ["ForStatement", "VariableDeclaration", "FunctionDeclaration"]) which must NOT be found in the ast. Importantly, reqs cannot repeat types. For example ["ForStatement", "ForStatement"] is not allowed.
+* Returns:
+** List of requirements that are present in the code. Empty list if none of the requirements are present in code.
 */
 var blacklist = function(ast, reqs) {
   /*
@@ -44,7 +48,7 @@ var blacklist = function(ast, reqs) {
   /*
   * matches starts off as an empty list. When we find a branch whose type is in reqs, add it to the list of matches and return the new list.
   */
-  var checkMatch = function(reqs, matches, type) {
+  var updateMatches = function(reqs, matches, type) {
     if (reqs.indexOf(type) != -1) {
       var matchesCopy = matches.slice(0);
       matchesCopy.push(type);
@@ -54,7 +58,7 @@ var blacklist = function(ast, reqs) {
   };
 
   var matches = [];
-  return dfs(ast, reqs, matches, checkMatch, checkDone);
+  return dfs(ast, reqs, matches, updateMatches, checkDone);
 };
 
 
@@ -65,6 +69,8 @@ var blacklist = function(ast, reqs) {
 {"type": "FunctionDeclaration",
  "body": {"type": "ReturnStatement"}
 }
+* Returns:
+** List of structures that are not present in the code. Empty list if all structure elements are present in code.
 */
 var structure = function(ast, reqs) {
   /*
@@ -86,12 +92,12 @@ var structure = function(ast, reqs) {
   /*
   * matches starts off as a copy of reqs. As we change the layer of the ast we examine, we also change the layer of reqs. When we find a leaf, we remove it from matches.
   */
-  var checkMatch = function(reqs, matches, type) {
+  var updateMatches = function(reqs, matches, type) {
     if (reqs.length) { // reqs is a list of nodes
       var reqsCopy = [];
       var matchesCopy = [];
       for (var i=0; i<reqs.length; i++) {
-        var result = checkMatch(reqs[i], matches[i], type);
+        var result = updateMatches(reqs[i], matches[i], type);
         reqsCopy[i] = result.reqs;
         matchesCopy[i] = result.matches;
       }
@@ -111,7 +117,7 @@ var structure = function(ast, reqs) {
   };
 
   var matches = $.extend(true, {}, reqs);
-  matches = dfs(ast, reqs, matches, checkMatch, checkDone);
+  matches = dfs(ast, reqs, matches, updateMatches, checkDone);
   if (checkDone(matches)) {
     return [];
   }
@@ -123,33 +129,35 @@ var structure = function(ast, reqs) {
 /*
 * dfs performs a depth first search on the ast.
 * Requires:
-** astBranch is a node within the ast. It must be a dictionary with one attribute "type".
+** node is a node within the ast. It must be a dictionary with one attribute "type". It optionally has an attribute "body", whose value is a single node or a list of nodes.
 ** reqs is a list of requirements the ast must have.
 ** matches is a list of types. It has a different purpose for whitelist, blacklist, and structure.
-** checkMatch is a function which checks whether we have reached a branch whose type is found in reqs. If so, it returns an updated matches.
+** updateMatches is a function which checks whether we have reached a branch whose type is found in reqs. If so, it returns an updated matches.
 ** checkDone is a function which checks whether dfs can terminate. It must return true or false.
+* Returns:
+** modified version of matches, according to the updateMatches function of the 
 */
-var dfs = function(astBranch, reqs, matches, checkMatch, checkDone) {
+var dfs = function(node, reqs, matches, updateMatches, checkDone) {
   // Check the whether the type of our branch is in reqs
-  var result = checkMatch(reqs, matches, astBranch.type);
+  var result = updateMatches(reqs, matches, node.type);
   var reqsCopy = result.reqs;
   var matchesCopy = result.matches;
   if (checkDone(matchesCopy)) {
     return matchesCopy;
   }
 
-  // If this is not a leaf, then astBranch will have a "body" attribute. Explore all of these.
-  if (astBranch.body) {
-    if (astBranch.body.length) { // List of children
-      for (var i=0; i<astBranch.body.length; i++) {
-        matchesCopy = dfs(astBranch.body[i], reqsCopy, matchesCopy, checkMatch, checkDone);
+  // If this is not a leaf, then node will have a "body" attribute. Explore all of these.
+  if (node.body) {
+    if (node.body.length) { // List of children
+      for (var i=0; i<node.body.length; i++) {
+        matchesCopy = dfs(node.body[i], reqsCopy, matchesCopy, updateMatches, checkDone);
         if (checkDone(matchesCopy)) {
           return matchesCopy;
         }
       }
     }
     else { // Child is a dictionary
-      matchesCopy = dfs(astBranch.body, reqsCopy, matchesCopy, checkMatch, checkDone);
+      matchesCopy = dfs(node.body, reqsCopy, matchesCopy, updateMatches, checkDone);
       if (checkDone(matchesCopy)) {
         return matchesCopy;
       }
